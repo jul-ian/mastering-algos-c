915aacffe7f6ddc40bffbb993789e6c125d406d8
jul-ian <julianaviles@gmail.com>
2025-07-26 15:42:06 -0700

    Finished list source code

--- list.c
+++ list.c
@@ -50,5 +50,26 @@ int list_rem_next(List *list, ListElmt *element, void **data) {
 
     ListElmt *old_element;
 
+    if(element == NULL) {
+        *data = list->head->data;
+        old_element = list->head;
+        list->head = list->head->next;
 
+        if(list_size(list) == 1) list->tail = NULL;
+    }
+    else {
+        if(element->next == NULL) return -1;
+
+        *data = element->next->data;
+        old_element = element->next;
+        element->next = element->next->next;
+
+        if(element->next == NULL) list->tail = element;
+    }
+
+    free(old_element);
+
+    list->size--;
+
+    return 0;
 }
