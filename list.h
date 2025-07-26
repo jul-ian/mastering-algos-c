afd9c8e38c5e7fb0838bbaf292ca9b93e9bdb291
jul-ian <julianaviles@gmail.com>
2025-07-26 11:31:18 -0700

    Finished list header file

--- list.h
+++ list.h
@@ -1,4 +1,40 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdlib.h>
+
+typedef struct ListElmt_ {
+    void *data;
+    struct ListElmt_ *next;
+} ListElmt;
+
+typedef struct List_ {
+    int size;
+    int (*match)(const void *key1, const void *key2);
+    ListElmt *head;
+    ListElmt *tail;
+} List;
+
+void list_init(List *list, void(*destroy)(void *data));
+
+void list_destroy(List *list);
+
+void list_ins_next(List *list, ListElmt *element, const void *data);
+
+int list_rem_next(List *list, ListElmt *element, void **data);
+
+#define list_size(list) ((list)->size)
+
+#define list_head(list) ((list)->head)
+
+#define list_tail(list) ((list)->tail)
+
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+
+#define list_is_tail(list, element) ((list)->next == NULL ? 1 : 0)
+
+#define list_data(list) ((list)->data)
+
+#define list_next(list) ((list)->next)
+
 #endif
