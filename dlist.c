88ad9701e203ecd5dbd627bf4f7f971d175cf7ff
jul-ian <julianaviles@gmail.com>
2025-07-27 12:07:31 -0700

    Working on dlist source code

---
+++ dlist.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "dlist.h"
+
+void dlist_init(DList *list, void (*destroy)(void *data)) {
+    list->size = 0;
+    list->destroy = destroy;
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+void dlist_destroy(DList *list) {
+    void *data;
+
+    while(dlist_size(list) > 0) {
+        if(dlist_remove(list, dlist_tail(list), (void **)&data) == 0 && list->destroy != NULL) {
+            list->destroy(data);
+        }
+    }
+    memset(list, 0, sizeof(DList));
+}
+
+int dlist_ins_next(DList *list, DListElmt *element, const void *data) {
+    if(element == NULL && dlist_size(list) != 0) return -1;
+
+    DListElmt *new_element;
+    if((new_element = (DListElmt *)malloc(sizeof(DListElmt))) == NULL) {
+        return -1;
+    }
+    new_element->data = (void *)data;
+
+    if(dlist_size(list) == 0) {
+        list->head = new_element;
+        list->head->prev = NULL;
+        list->head->next = NULL;
+        list->tail = new_element;
+    } else {
+        new_element->next = element->next;
+        new_element->prev = element;
+
+        if(element->next == NULL) {
+            list->tail = new_element;
+        }
+        else {
+            element->next->prev = new_element;
+        }
+
+        element->next = new_element;
+    }
+
+    list->size++;
+
+    return 0;
+}
+
