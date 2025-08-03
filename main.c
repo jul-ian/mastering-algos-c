d4214343ca1b1098eea3a2d2ef8809ef908d01ca
jul-ian <julianaviles@gmail.com>
2025-08-03 16:34:42 -0700

    Working on set example

--- main.c
+++ main.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 
-#include "list.h"
+#include "set.h"
+
+typedef struct {
+    int id_num;
+    int evil_level;
+    char *saying;
+} Imp;
+
+int imp_match(const void *key1, const void *key2) {
+    Imp *imp1 = (Imp *)key1;
+    Imp *imp2 = (Imp *)key2;
+
+    return (imp1->id_num) == (imp2->id_num);
+}
 
 int main() {
-    List *datalist = (List *)malloc(sizeof(List));
-    if(datalist == NULL) return -1;
+    Set *imp_set;
+    set_init(imp_set, imp_match, free);
+    /*
+    for(int i = 0; i < 5; i++) {
+        Imp *current_imp = (Imp *)malloc(sizeof(Imp));
+        current_imp->id_num = 1000 + i;
+        current_imp->evil_level = (666 - 2*i);
+        sprintf(current_imp->saying, "I am imp #%d\n", current_imp->id_num);
 
-    list_init(datalist, free);
-
-    int *ins_int;
-    for(int i = 5; i > 0; i--) {
-        ins_int = (int *)malloc(sizeof(int));
-        *ins_int = i;
-        list_ins_next(datalist, NULL, ins_int);
+        set_insert(imp_set, (void *)current_imp);
     }
-
-    ListElmt *element = list_head(datalist);
-    while(element != NULL) {
-        printf("%d\n", *(int *)(list_data(element)));
-        element = list_next(element);
-    }
-    list_destroy(datalist);
-    free(datalist);
+     */
+    Imp *current_imp = (Imp *)malloc(sizeof(Imp));
+    current_imp->id_num = 1000 + 1;
+    current_imp->evil_level = (666 - 2*1);
+    //sprintf(current_imp->saying, "I am imp #%d\n", current_imp->id_num);
+    current_imp->saying = "hello";
+    printf("%d\n", set_size(imp_set));
+    set_destroy(imp_set);
     return 0;
 }
