6bab56115b36fdb583d25c460e5d2ce05049650c
jul-ian <julianaviles@gmail.com>
2025-09-27 10:30:51 -0700

    Finished header file for chained hash table

--- chtbl.h
+++ chtbl.h
@@ -15,4 +15,17 @@ typedef struct CHTbl_ {
     int size;
     List *table;
 } CHTbl;
+
+int chtbl_init(CHTbl *htbl, int buckets, int (*h)(const void *key), int (*match)(const void *key1, const void *key2), void (*destroy)(void *data));
+
+void chtbl_destroy(CHTbl *htbl);
+
+int chtbl_insert(CHTbl *htbl, const void *data);
+
+int chtbl_remove(CHTbl *htbl, void **data);
+
+int chtbl_lookup(const CHTbl *htbl, void **data);
+
+#define chtbl_size ((htbl)->size)
+
 #endif
