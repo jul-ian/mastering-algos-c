6485f2494f2887ede31b507c6029d24d9ac39515
jul-ian <julianaviles@gmail.com>
2025-10-02 22:06:51 -0700

    Finished header file for open-addressed hash table

--- ohtbl.h
+++ ohtbl.h
@@ -16,4 +16,16 @@ typedef struct OHTbl_ {
     void **table;
 } OHTbl;
 
+int ohtbl_init(OHTbl *htbl, int positions, int (*h1)(const void *key), int (*h2)(const void *key), int (*match)(const void *key1, const void *key2), void (*destroy)(void *data));
+
+void ohtbl_destroy(OHTbl *htbl);
+
+int ohtbl_insert(OHTbl *htbl, const void *data);
+
+int ohtbl_remove(OHTbl *htbl, void **data);
+
+int ohtbl_lookup(const OHTbl *htbl, void **data);
+
+#define ohtbl_size(htbl) ((htbl)->size)
+
 #endif
