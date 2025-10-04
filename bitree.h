8167e51a6f148177ed1ec5375899cfb35b874ef8
jul-ian <julianaviles@gmail.com>
2025-10-04 10:12:25 -0700

    Started header file for binary tree

---
+++ bitree.h
@@ -0,0 +1,21 @@
+#ifndef BITREE_H
+#define BITREE_H
+
+#include <stdlib.h>
+
+typedef struct BiTreeNode_ {
+    void *data;
+    struct BiTreeNode_ *left;
+    struct BiTreeNode_ *right;
+} BiTreeNode;
+
+typedef struct BiTree_ {
+    int size;
+
+    int (*compare)(const void *key1, const void *key2);
+    void (*destroy)(void *data);
+
+    BiTreeNode *root;
+} BiTree;
+
+#endif
