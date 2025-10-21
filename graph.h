c5fa0af85f0df3c8318bcf159ba48e0062235aed
jul-ian <julianaviles@gmail.com>
2025-10-20 22:10:31 -0700

    Finished header file for graph

--- graph.h
+++ graph.h
@@ -21,5 +21,28 @@ typedef struct Graph_ {
     List adjlists;
 } Graph;
 
+typedef enum VertexColor_ {white, gray, black} VertexColor;
+
+void graph_init(Graph *graph, int (*match)(void *key1, void *key2), void (*destroy)(void *data));
+
+void graph_destroy(Graph *graph);
+
+int graph_ins_vertex(Graph *graph, const void *data);
+
+int graph_ins_edge(Graph *graph, const void *data1, const void *data2);
+
+int graph_rem_vertex(Graph *graph, void **data);
+
+int graph_rem_edge(Graph *graph, void *data1, void **data2);
+
+int graph_adjlist(const Graph *graph, const void *data, AdjList **adjlist);
+
+int graph_is_adjacent(Graph *graph, const void *data1, const void *data2);
+
+#define graph_adjlists(graph) ((graph)->adjlists)
+
+#define graph_vcount(graph) ((graph)->vcount)
+
+#define graph_ecount(graph) ((graph)->ecount)
 
 #endif
