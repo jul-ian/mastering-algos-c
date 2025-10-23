57c6da7bc625d2bf11f2c10037ee9b2f8fc58099
jul-ian <julianaviles@gmail.com>
2025-10-22 22:29:06 -0700

    Finished source file for graph implementation

--- graph.c
+++ graph.c
@@ -175,5 +175,19 @@ int graph_adjlist(const Graph *graph, const void *data, AdjList **adjlist) {
 }
 
 int graph_is_adjacent(const Graph *graph, const void *data1, const void *data2) {
+    ListElmt *element, *prev;
 
+    prev = NULL;
+
+    for(element = list_head(&graph->adjlists); element != NULL; element = list_next(element)) {
+        if(graph->match(data1, ((AdjList *)list_data(element))->vertex)) {
+            break;
+        }
+
+        prev = element;
+    }
+
+    if(element == NULL) return 0;
+
+    return set_is_member(&((AdjList *)list_data(element))->adjacent, data2);
 }
